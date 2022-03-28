b34ce1b4be80b832026dada96d2e52f724865356
Ahmed Sobhy <eng.ahmedsobhy2019@gmail.com>
2022-03-28 13:31:14 +0200

    second

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <footer className="Footer">     
+    </footer>
+  );
+}
+
+export default Footer;
